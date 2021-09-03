383dce403c93b18791f8d11b63f4632592bc7999
Connor Hansen <chansen13@georgefox.edu>
2021-09-03 08:31:20 -0700

    add a failing test to onMercury. Will write method.

--- __tests__/age-calc.test.js
+++ __tests__/age-calc.test.js
@@ -3,7 +3,12 @@ import AgeCalc from './../src/js/age-calc.js';
 describe('AgeCalc', () => {
   
   test('Should create a new instance of an Age Calculator', () => {
-    const newCalc = new AgeCalc();
-    expect(newCalc.age).toEqual();
-  });
+      let newCalc = new AgeCalc(50);
+      expect(newCalc.age).toEqual(50);
+    });
+    
+    test('Should calculate age on Mercury', () => {
+        const newCalc = new AgeCalc(1);
+        expect(newCalc.onMercury(newCalc.age)).toEqual(4.16);
+      });
 });
