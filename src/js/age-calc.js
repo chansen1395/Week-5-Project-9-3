6b779327eae46ebb014c4b1eedc5b01b446d3a7f
Connor Hansen <chansen13@georgefox.edu>
2021-09-03 09:56:29 -0700

    WIP, will refactor after main objectives are complete.

--- src/js/age-calc.js
+++ src/js/age-calc.js
@@ -4,7 +4,6 @@ export default class AgeCalc {
     this.yrsLeft = lifeExpectancy;
   }
 
-// Note: try using an object to pass into the constructor for age to refactor
 
   lifeExpec() {
     this.yrsLeft = (72 - this.age);
@@ -18,7 +17,6 @@ export default class AgeCalc {
 
   expecMercury() {
     this.yrsLeft = (this.lifeExpec() / .24).toFixed(2);
-    // this.age = ((this.age / .24).toFixed(2));
     return parseFloat(this.yrsLeft);
   }
 }
---
+++ src/js/age-calc2.js
@@ -0,0 +1,13 @@
+// export default class AgeCalc2 {
+// constructor(ageMap) {
+//   this.ageMap = ageMap;
+// }
+
+// // Note: try using an object or map to pass into the constructor for age to refactor
+// ageMap = new Map(
+//   [
+//     [age, 0]
+//     // [yrsLeft, 0]
+//   ]
+// );
+// }
